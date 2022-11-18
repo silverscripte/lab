a4bce493fa50a1f39d71856463115e6e87704a23
silverscripte <74472805+silverscripte@users.noreply.github.com>
2022-11-18 17:36:41 -0600

    added docstrings and type hinting to account.py

--- account.py
+++ account.py
@@ -1,11 +1,23 @@
 class Account:
+    '''
+    A class representing details for an account object
+    '''
 
-    def __init__(self, name):
+    def __init__(self, name: str) -> None:
+        '''
+        Constructor to set up initial state of the account object
+        :param name: Name of account
+        '''
         self.__account_name = name
         self.__account_balance= 0
 
 
-    def deposit(self, amount):
+    def deposit(self, amount: int) -> bool:
+        '''
+        Method to add money to account_balance
+        :param amount: The amount to be added to the balance if it is positive
+        :return: True if it worked, False if it did  not
+        '''
         if amount >= 0:
             self.__account_balance += amount
             return True
@@ -13,7 +25,12 @@ class Account:
         else:
             return False
 
-    def withdraw(self, amount):
+    def withdraw(self, amount: int) -> bool:
+        '''
+        Method to reduce money in the account_balance
+        :param amount: The amount to be taken out of the account
+        :return: False if no money was withdrawn, True if money was withdrawn
+        '''
         if amount < 0 or (amount > self.__account_balance):
             return False
 
@@ -21,12 +38,16 @@ class Account:
             self.__account_balance -= amount
             return True
 
-    def get_balance(self):
+    def get_balance(self) -> int:
+        '''
+        Method to access the balance of the account
+        :return: int value of account_balance
+        '''
         return self.__account_balance
 
-    def get_name(self):
+    def get_name(self) -> str:
+        '''
+        Method to access the name of the account
+        :return: the string name of account_name
+        '''
         return self.__account_name
-
-
-
-
