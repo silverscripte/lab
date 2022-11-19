e45062e79c1fc5eb0c72868a74bbd2500f45c689
silverscripte <74472805+silverscripte@users.noreply.github.com>
2022-11-18 18:51:49 -0600

    added the deposit and withdraw tests and get_balance to the test_init

--- account.py
+++ account.py
@@ -9,36 +9,36 @@ class Account:
         :param name: Name of account
         '''
         self.__account_name = name
-        self.__account_balance= 0
+        self.__account_balance = 0
 
 
-    def deposit(self, amount: int) -> bool:
+    def deposit(self, amount: float) -> bool:
         '''
         Method to add money to account_balance
         :param amount: The amount to be added to the balance if it is positive
         :return: True if it worked, False if it did  not
         '''
-        if amount >= 0:
+        if amount > 0:
             self.__account_balance += amount
             return True
 
         else:
             return False
 
-    def withdraw(self, amount: int) -> bool:
+    def withdraw(self, amount: float) -> bool:
         '''
         Method to reduce money in the account_balance
         :param amount: The amount to be taken out of the account
         :return: False if no money was withdrawn, True if money was withdrawn
         '''
-        if amount < 0 or (amount > self.__account_balance):
+        if amount <= 0 or (amount > self.__account_balance):
             return False
 
         else:
             self.__account_balance -= amount
             return True
 
-    def get_balance(self) -> int:
+    def get_balance(self) -> float:
         '''
         Method to access the balance of the account
         :return: int value of account_balance
--- test_account.py
+++ test_account.py
@@ -1,4 +1,4 @@
-import pytest
+from pytest import *
 from account import *
 
 class Test:
@@ -14,13 +14,64 @@ class Test:
         assert self.a1.get_name() == 'Jane'
         assert self.a2.get_name() == 'John'
 
+        assert self.a1.get_balance() == 0
+        assert self.a2.get_balance() == 0
+
 
 
     def test_deposit(self):
-        pass
+        #a1 test
+        assert self.a1.deposit(30) is True
+        assert self.a1.get_balance() == 30
+
+        assert self.a1.deposit(-30) is False
+        assert self.a1.get_balance() == 30
+
+        assert self.a1.deposit(0) is False
+        assert self.a1.get_balance() == 30
+
+        #a2 test
+        assert self.a2.deposit(0) is False
+        assert self.a2.get_balance() == 0
+
+        assert self.a2.deposit(-10) is False
+        assert self.a2.get_balance() == 0
+
+        assert self.a2.deposit(12) is True
+        assert self.a2.get_balance() == 12
 
     def test_withdraw(self):
-        pass
+        #a1 test
+        assert self.a1.withdraw(0) is False
+        assert self.a1.get_balance() == 0
+
+        self.a1.deposit(30)
+
+        assert self.a1.withdraw(10) is True
+        assert self.a1.get_balance() == 20
+
+        assert self.a1.withdraw(-10) is False
+        assert self.a1.get_balance() == 20
+
+        assert self.a1.withdraw(30) is False
+        assert self.a1.get_balance() == 20
+
+        #a2 test
+        self.a2.deposit(12)
+
+        assert self.a2.withdraw(2) is True
+        assert self.a2.get_balance() == 10
+
+        assert self.a2.withdraw(0) is False
+        assert self.a2.get_balance() == 10
+
+        assert self.a2.withdraw(-2) is False
+        assert self.a2.get_balance() == 10
+
+        assert self.a2.withdraw(15) is False
+        assert self.a2.get_balance() == 10
+
+
 
 
 
